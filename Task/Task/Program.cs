a3ab05f48650355a02ca9fc4a5795bd38917195c
Sananhuseyinzadeh <sananeh@code.edu.az>
2023-04-09 22:47:38 +0300

    first commit

---
+++ Task/Task/Program.cs
@@ -0,0 +1,5 @@
+﻿Student student1 = new Student("Sanan","Huseynzade", "P234",100,true);
+
+student1.GetFullName();
+student1.GetFullInfo();
+student1.NextExam();
---
+++ Task/Task/Student.cs
@@ -0,0 +1,50 @@
+﻿
+internal class Student
+{
+    public string Name;
+    public string SurName;
+    public string Group;
+    public double Point;
+    public bool IsGraduate;
+
+    public Student(string name, string SurName, string group, double point, bool IsGraduate)
+    {
+        this.Name = name;
+        this.SurName = SurName;
+        this.Group = group;
+        this.Point = point;
+        this.IsGraduate = IsGraduate;
+    }
+    public void GetFullName()
+    {
+        Console.WriteLine($"{this.Name},{this.SurName}");
+    }
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"{this.Name},{this.SurName},{this.Group},{this.Point},{this.IsGraduate}");
+        if (IsGraduate == true)
+        {
+            Console.WriteLine("Mezun oldu");
+        }
+        else 
+        {
+            Console.WriteLine("Mezun ola bilmedi");
+        }
+    }
+    public void NextExam() 
+    {
+        if (IsGraduate == false && Point > 80)
+        {
+            Console.WriteLine("Yeniden imtahana gire biler");
+        }
+        else 
+        {
+            Console.WriteLine("Yeniden imtahana gire bilmez");
+        }
+    
+    }
+    
+
+
+    }
+
